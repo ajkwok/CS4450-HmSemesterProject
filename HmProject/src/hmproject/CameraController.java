15935c8de367c8eba07145405a378103b717a1c2
ajkwok <35111853+ajkwok@users.noreply.github.com>
2020-10-25 15:06:20 -0700

    add terrain.png

--- HmProject/src/hmproject/CameraController.java
+++ HmProject/src/hmproject/CameraController.java
@@ -215,11 +215,9 @@ public class CameraController {
             camera.lookThrough();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             
-            render();
-            /*????
+            //render();
             Chunk chunk = new Chunk(0, 0, 0);
             chunk.render();
-            ???*/
             
             Display.update();
             Display.sync(60);
